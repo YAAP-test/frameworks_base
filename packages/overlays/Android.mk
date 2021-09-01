bc2857918e6a525bf34b4677a98b473548c56b0a
AshutoshSundresh <62063875+AshutoshSundresh@users.noreply.github.com>
2021-09-01 15:38:28 +0530

    overlays: Add OxygenOS {Settings, Launcher, Theme Picker} placeholders
    This is probably why the OxygenOS theme doesn't properly set after reboot after https://github.com/ShapeShiftOS/android_frameworks_base/commit/4eb1453e75fee52470729be50eddbc2c0df4fdd8
    
    Signed-off-by: Sagarrokade006 <Sagarrokade006@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -58,6 +58,9 @@ LOCAL_REQUIRED_MODULES := \
     IconPackRoundedThemePickerOverlay \
     IconPackOOSAndroidOverlay \
     IconPackOOSSystemUIOverlay \
+    IconPackOOSLauncherOverlay \
+    IconPackOOSSettingsOverlay \
+    IconPackOOSThemePickerOverlay \
     NavigationBarMode3ButtonOverlay \
     NavigationBarModeGesturalOverlay \
     NavigationBarModeGesturalOverlayNarrowBack \
